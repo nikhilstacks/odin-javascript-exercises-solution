9da4ea1278eb82cbf1319c21b6d89acf6209cfcf
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 01:00:39 +0530

    answer completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 100 == 0){    //if year is divisible by 100
+     if(year % 400 == 0){   //then check year is divisible by 400
+      return true;
+     }
+     else {
+        return false;
+    } 
+    } if(year % 4 == 0){    //if year gives 0 reminder on divisible by 4
+        return true;
+    } else {
+        return false;
+    }
+    
 };
-
+    console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
