3225800c4d73abf57760e0c7e2f2bbce97d690bd
nikhilstacks <nikhilstacks@gmail.com>
2022-02-04 23:54:15 +0530

    answer completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+      const newArr = string.split(""); //best practice to not alter main input string
+      console.log(newArr);  //check by logging the value of newArr
+      const answer = []; //new array where our all new values are going to save
+      
+      for(let i = newArr.length - 1;i >= 0; i--){
+          answer.push(newArr[i]);      //pushing value of newArr from last
+      }
+      return answer.join(''); //returning array to string back
 };
 
 // Do not edit below this line
