3ce30034b89205447dbdc0bc0bec3dfdd6b1b959
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 01:18:21 +0530

    answer completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+     return parseFloat(((temp - 32) * 5/9).toFixed(1)); 
 };
-
-const ctof = function() {
-
+//converting with respective furmulae with fixed one decimal but to fixed convert result to string
+//that is why use of parseFloat to convert string into float 
+//******* important dont use parseInt it will ommit the decimal value
+const ctof = function(temp) {
+     return parseFloat((temp * 9/5 + 32).toFixed(1));
 };
-
+   console.log(ctof(73.2));
 // Do not edit below this line
 module.exports = {
   ftoc,
