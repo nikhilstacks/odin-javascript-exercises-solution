0f176850d2af9112cfd97d22b5755e214d8d288f
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 00:42:32 +0530

    answer completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(rangeMin, rangeMax) {
+    let sum = 0;
+    if(rangeMin > rangeMax){ //if the value of first parameter if bigger swap the value
+        let temp = rangeMin;
+        rangeMin = rangeMax;
+        rangeMax = temp;
+    }
+    if(typeof rangeMax == "number" && typeof rangeMin == "number" && rangeMin > 0 && rangeMax > 0){
+        for(let i = rangeMin; i<= rangeMax; i++){  //checking input is a positive number
+            sum += rangeMin;
+            rangeMin++;
+        }
+    }  else {
+        return "ERROR";
+    }
+      return sum;
 };
+    console.log(sumAll(4,1));
 
 // Do not edit below this line
 module.exports = sumAll;
