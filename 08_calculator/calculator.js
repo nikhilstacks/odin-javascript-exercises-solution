972387d87aec6d901906e0775aa8cc81bd4ee409
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 13:55:06 +0530

    answer completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+      const numbersToSum = num;
+      console.log(numbersToSum);
+      let sum = 0;
+	    if(numbersToSum.length == 0){
+        return 0;
+      } else {
+          for(let i = 0; i < numbersToSum.length; i++){ 
+            sum += numbersToSum[i];
+            }
+        }
+        return sum;
+};
+console.log(sum([1,2,3]));
+
+const multiply = function(num) {
+      const numbersToMultiply = num;
+      let answer = 1;
+      for(let i = 0; i < numbersToMultiply.length; i++){
+        answer *= numbersToMultiply[i];
+      }
+      return answer;
+
+};
+console.log(multiply([3,2]));
+
+const power = function(num, exp) {
+	  let answer  = 1;    
+    while(exp > 0){
+        answer *= num;
+        exp--;
+      }
+      return answer;
 };
 
-const multiply = function() {
-
+const factorial = function(num) {
+	  if(num == 0 || num == 1){
+      return 1;
+    } else {
+      return num * factorial(num - 1);
+    }
 };
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
