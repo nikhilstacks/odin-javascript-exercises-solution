07cb4366b88eaf124cba6f976df75dacae47b58b
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 20:33:21 +0530

    answer completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles =  [];
+     for(let i = 0; i< books.length;i++ ){
+         titles.push(books[i].title)   //using the . operator to access the title key in array of objects
+     }
+     return titles;
 };
-
+console.log(getTheTitles([
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]));
 // Do not edit below this line
 module.exports = getTheTitles;
