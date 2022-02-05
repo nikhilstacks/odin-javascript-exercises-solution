8c58e1d65c966560402a4ffea6c06bee005db691
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 15:07:37 +0530

    answer completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let computeString = str.replace(/\s/g,"").replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    console.log(computeString);
+    let count = 0;
+    for(let i = 0; i< computeString.length; i++){
+        for(j = computeString.length - (i + 1); j >=0; j--){
+            if(computeString[i] == computeString[j]){
+                count++;
+            } 
+            break;
+        }
+    } 
+    if(count == computeString.length){
+        console.log(count);
+        return true;
+    } else {
+        console.log(count);
+        return false;
+    }
 };
-
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
