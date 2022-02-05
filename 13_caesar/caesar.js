1d5f968aa05bd67d5707a6a63dfe0d77dc65d161
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 22:58:10 +0530

    complex

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,52 @@
-const caesar = function() {
-
-};
+const caesar = function(string, num) {
+    let arr = [];
+    for(let i=0;i<string.length;i++)
+      {
+          if(!(/[a-zA-Z]/.test(string[i])))
+        {
+            arr[i]=string[i]; 
+            continue;
+        }
+      let n = string.charCodeAt(i) + num;
+      if (string[i] == string[i].toLowerCase()) 
+        {
+            if(n>122)  
+          {
+              while(n>122)
+            {
+                n-=26;
+            }
+        } 
+         else  
+          {
+              while(n<97)
+            {
+                n+=26;
+            }
+          }
+        }
+      else
+        {
+            if(n>90)
+          {
+              while(n>90)
+            {
+                n-=26;
+            }
+          } 
+         else 
+          {
+              while(n<65)
+            {
+                n+=26;
+            }
+          }
+        }
+      arr[i]=String.fromCharCode(n);    
+      }
+    return arr.join('');
+    }
+console.log(caesar('Hello, World!', -29));
 
 // Do not edit below this line
 module.exports = caesar;
