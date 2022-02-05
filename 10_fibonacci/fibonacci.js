37e676909e29a02a8f9f5f244a5e9c45bbe6c751
nikhilstacks <nikhilstacks@gmail.com>
2022-02-05 20:23:25 +0530

    answer completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fab = [1,1];    //first two element are fixed 1 and 1
+    
+    if(num < 0){
+        return 'OOPS';    //if negative return OOPS
+    }
+    for(let i = 2; i < num;i++ ){
+       let temp = 0;
+       temp = fab[i - 1] + fab[i - 2];    //sum of previous two elements and push them into array fab
+       fab.push(temp);
+    }
+    return fab[num - 1];  //returning the num(th) element because we have to specify index so num - 1
 };
-
+console.log(fibonacci(8));
 // Do not edit below this line
 module.exports = fibonacci;
